af6141c223c2738b144b73b9cc48da80843c7ebf
Abhra303 <chakrabortyabhradeep79@gmail.com>
2023-04-08 11:38:11 +0530

    feat: implement client create and join room facility

--- cmd/client.go
+++ cmd/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/Abhra303/Rank-Rumble/pkg/room"
 	"github.com/gorilla/websocket"
@@ -16,10 +17,13 @@ type requestMessage struct {
 }
 
 type responseMessage struct {
+	Err    string `json:"error"`
+	Status int    `json:"status"`
 }
 
 type client struct {
 	conn     *websocket.Conn
+	room     room.Room
 	writeMsg chan responseMessage
 }
 
@@ -41,20 +45,47 @@ func (c *client) listenRead() {
 		err := c.conn.ReadJSON(&m)
 		if err != nil {
 			log.Println(err)
+			c.writeMsg <- responseMessage{Err: err.Error(), Status: http.StatusBadRequest}
 			return
 		}
 		if m.CreateRoom {
+			// code for room creation
 			if m.MaxPlayerLimit == 0 {
 				m.MaxPlayerLimit = room.DefaultRoomLimit
 			}
-			// code for room creation
+			rm := room.NewRoom(c.conn, m.MaxPlayerLimit)
+			err = AvailableRooms.AddRoom(rm)
+			if err != nil {
+				log.Println(err)
+				c.writeMsg <- responseMessage{Err: err.Error(), Status: http.StatusInternalServerError}
+				return
+			}
+			c.room = rm
+			c.writeMsg <- responseMessage{Err: "", Status: http.StatusCreated}
 		} else if m.JoinRoomIfMatch {
 			// for each elem in room see the condition
 			// if satisy, join into the room
+			if c.room != nil {
+				c.writeMsg <- responseMessage{Err: "client already aligned with a room", Status: http.StatusBadRequest}
+				return
+			}
+			if m.MaxPlayerLimit == 0 {
+				m.MaxPlayerLimit = room.DefaultRoomLimit
+			}
+
+			rm, err := AvailableRooms.MatchRoom(c.conn, m.MaxPlayerLimit)
+			if err != nil {
+				log.Println(err)
+				c.writeMsg <- responseMessage{Err: err.Error(), Status: http.StatusInternalServerError}
+			}
+			c.room = rm
+			c.writeMsg <- responseMessage{Err: "", Status: http.StatusOK}
 		} else if m.StartGame {
 			// code for starting the game
+			return
 		} else if m.EndGame {
 			// code for ending game
+			return
 		}
 	}
 }
--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/Abhra303/Rank-Rumble/pkg/room"
 )
 
+var AvailableRooms room.RoomPool
+
 func main() {
+	AvailableRooms = room.NewRoomPool()
 	http.HandleFunc("/", CreateSocketHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
